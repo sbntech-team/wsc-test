7c049e391190ebc50881f3055e34a588f92c2e7a
Fan Syong Yue <mastersyong@gmail.com>
2020-09-07 14:24:21 +0800

    init

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questionsObj = [
+  {
+    name: "General Surrounding Area",
+    questions: [
+      {
+        name:
+          "Is the area generally kept tidy, clean regularly & is well maintained?",
+        letter: "a",
+        subQuestions: [
+          {
+            qn: "Walls – e.g. look out for stains, cracks",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+    ],
+  },
+];
