9fc1076a31c9d3590b0d81cd975b8f1be668c67d
Fan Syong Yue <mastersyong@gmail.com>
2020-09-11 10:17:17 +0800

    section 1 question

--- questions.js
+++ questions.js
@@ -13,6 +13,159 @@ const questionsObj = [
             actions: null,
             by: null,
           },
+          {
+            qn: "Roads / Floors / Drains – e.g. cracks, choking drain",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn: "Floors / Drain covers – e.g. broken or absence",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn: "Ceilings / Ceiling boards e.g. pooling of water, stains",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn:
+              "Ventilation ducts / vents (includes air-conditioning systems) – e.g. dirty with dust, condensation present",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn: "Main rubbish disposal point",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn:
+              "Stairs e.g. slip resistant, presence of handrails when required",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn: "Any overhead hazards? E.g. wires, trees, lamp post, etc.",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+      {
+        name:
+          "Are there guardrails, toe boards provided at adequate places? E.g. elevated ground, floor openings, etc.",
+        letter: "b",
+        subQuestions: [
+          {
+            qn: "",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+      {
+        name:
+          "Are floor openings guarded by a cover, a guardrails / equivalent on all sides (Except at entrance to stairways / ladders) available?",
+        letter: "c",
+        subQuestions: [
+          {
+            qn: "",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+      {
+        name:
+          "Are grates or similar type covers over floor openings such as floor drains available?",
+        letter: "d",
+        subQuestions: [
+          {
+            qn: "",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+      {
+        name:
+          "Lighting – stairway, passageways and landings should be brightly lit",
+        letter: "e",
+        subQuestions: [
+          {
+            qn: "",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+      {
+        name: "Are there adequate warning signs available?",
+        letter: "f",
+        subQuestions: [
+          {
+            qn: "Traffic directional signs (road / pedestrians).",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn:
+              "Buildings / infra for standby generators, electrical riser, etc..",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn: "The location where contractors are working",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+      {
+        name:
+          "Are emergency landmarks free from hazards, obstructions and maintained?",
+        letter: "g",
+        subQuestions: [
+          {
+            qn: "Firefighting equipment e.g. fire hydrants / hose.",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+          {
+            qn: "Assembly areas used for fire drills",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
+        ],
+      },
+      {
+        name:
+          "Are all the Workplace Safety Inspection reports retained for at least ONE year?",
+        letter: "h",
+        subQuestions: [
+          {
+            qn: "",
+            answerBool: null,
+            actions: null,
+            by: null,
+          },
         ],
       },
     ],
